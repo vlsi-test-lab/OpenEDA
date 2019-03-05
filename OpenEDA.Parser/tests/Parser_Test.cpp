d303819eeeeb063829d2009a0581c6496ffbf916
Joshua Immanuel <jzi0005@tigermail.auburn.edu>
2019-03-05 22:33:34 +0000

    Imported from Changeset 257

--- OpenEDA.Parser/tests/Parser_Test.cpp
+++ OpenEDA.Parser/tests/Parser_Test.cpp
@@ -13,38 +13,43 @@
 #include"Circuit.h"
 #include <unordered_set>
 
-//Circuit* Parse(std::string _filePath);
-TEST(ParserTest, TEST01) {
+class ParserTest : public ::testing::Test {
+public:
+	void SetUp() override {
+
+	}
 	Parser parse;
+	Circuit* c = parse.Parse("c17.bench");
+	std::unordered_set<Levelized*> nodes = c->nodes();
+	std::unordered_set<Levelized*> pis = c->pis();
+	std::unordered_set<Levelized*> pos = c->pos();
+};
+//Circuit* Parse(std::string _filePath);
+TEST_F(ParserTest, TEST01) {
+	
 	EXPECT_ANY_THROW(Circuit* c = parse.Parse("c18.bench"););
 }
 
 //Circuit* Parse(std::string _filePath);
-TEST(ParserTest, TEST02) {
-	Parser parse;
+TEST_F(ParserTest, TEST02) {
+	
 	EXPECT_NO_THROW(Circuit* c = parse.Parse("c17.bench"););
 }
 
 //Circuit* Parse(std::string _filePath);
-TEST(ParserTest, TEST03) {
-	Parser parse;
-	Circuit* c = parse.Parse("c17.bench");
-	std::unordered_set<Levelized*> nodes = c->nodes();
+TEST_F(ParserTest, TEST03) {
+
 	EXPECT_EQ(13,nodes.size());
 }
 
 //Circuit* Parse(std::string _filePath);
-TEST(ParserTest, TEST04) {
-	Parser parse;
-	Circuit* c = parse.Parse("c17.bench");
-	std::unordered_set<Levelized*> pis = c->pis();
+TEST_F(ParserTest, TEST04) {
+
 	EXPECT_EQ(5, pis.size());
 }
 
 //Circuit* Parse(std::string _filePath);
-TEST(ParserTest, TEST05) {
-	Parser parse;
-	Circuit* c = parse.Parse("c17.bench");
-	std::unordered_set<Levelized*> pos = c->pos();
+TEST_F(ParserTest, TEST05) {
+
 	EXPECT_EQ(2, pos.size());
 }
