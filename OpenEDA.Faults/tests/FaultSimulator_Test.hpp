af72d3d0fee96e2ca949948a190bbb50ca9d677b
Joshua Immanuel <jzi0005@tigermail.auburn.edu>
2019-03-07 15:57:00 +0000

    Imported from Changeset 260

---
+++ OpenEDA.Faults/tests/FaultSimulator_Test.hpp
@@ -0,0 +1,59 @@
+/**
+ * @file COPStructures_Tests.cpp
+ * @author Joshua Immanuel (jzi0005@tigermail.auburn.edu)
+ * @version 0.1
+ * @date 2019-03-01
+ *
+ * @copyright Copyright (c) 2019
+ *
+ */
+
+#include"gtest/gtest.h"
+#include"FaultSimulator.h"
+#include"FaultGenerator.h"
+#include"Parser.h"
+
+
+class FaultSimTest : public ::testing::Test {
+public:
+	void SetUp() override {
+
+	}
+	Value<bool> val0 = new Value<bool>(0);
+	Value<bool> val1 = new Value<bool>(1);
+	Parser parser;
+	Circuit* c = parser.Parse("c17.bench");
+	Circuit* and = parser.Parse("and.bench");
+	std::vector<Value<bool>> invalid = { val0,val0};
+	std::vector<Value<bool>> invals0 = { val0,val0,val0,val0,val0 };
+	std::vector<Value<bool>> invals1 = { val1,val1,val1,val1,val1 };
+	std::vector<Value<bool>> outvals = { };
+	std::vector<Value<bool>> zero_check = { val0, val1};
+	FaultSimulator<bool> fsim;
+	FaultGenerator<bool> fgen;
+	std::unordered_set<Fault<bool>*> faults = fgen.allFaults(c);
+	std::unordered_set<Fault<bool>*> andfaults = fgen.allFaults(and);
+};
+
+TEST_F(FaultSimTest, ApplyStimulus01) {
+	
+	EXPECT_ANY_THROW(fsim.applyStimulus(c, invalid););
+}
+
+TEST_F(FaultSimTest, ApplyStimulus02) {
+
+	outvals = fsim.applyStimulus(c, invals0);
+	EXPECT_TRUE(outvals.begin(),outvals.end(),zero_check.begin());
+}
+
+TEST_F(FaultSimTest, SetFaults01) {
+
+	EXPECT_NO_THROW(fsim.setFaults(faults));
+}
+
+TEST_F(FaultSimTest, SetFaults02) {
+
+	fsim.setFaults(andfaults);
+	fsim.applyStimulus(and,invalid);
+	EXPECT_EQ(2,fsim.detectedFaults().size());
+}
