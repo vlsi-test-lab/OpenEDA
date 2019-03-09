0dcceea1ed9904ba0456e78acd158651530d20e8
Joshua Immanuel <jzi0005@tigermail.auburn.edu>
2019-03-09 02:16:10 +0000

    Imported from Changeset 264

--- OpenEDA.Faults/tests/FaultyStructure_Test.hpp
+++ OpenEDA.Faults/tests/FaultyStructure_Test.hpp
@@ -11,21 +11,28 @@
 #include"gtest/gtest.h"
 #include"Fault.h"
 
+class FaultyLineTest : public ::testing::Test {
+public:
+	void SetUp() override {
+
+	}
+	FaultyLine<bool> fl;
+	std::string name = fl.name();
+	FaultyLine<bool>* flnamed = new FaultyLine<bool>("Test_Line");
+};
+
  //FaultyLine();
-TEST(FaultyLine_Constructor_Test, TEST01) {
+TEST_F(FaultyLineTest, ConstructorTEST01) {
 	EXPECT_NO_THROW(FaultyLine<bool>fl;);
 }
 
 //FaultyLine();
-TEST(FaultyLine_Constructor_Test, TEST02) {
-	FaultyLine<bool> fl;
-	std::string name = fl.name();
+TEST_F(FaultyLineTest, ConstructorTEST02) {
 	EXPECT_EQ("UNDEF", name);
 }
 
 //FaultyLine();
-TEST(FaultyLine_Constructor_Test, TEST03) {
-	FaultyLine<bool> fl("Test_Line");
-	std::string name = fl.name();
+TEST_F(FaultyLineTest, ConstructorTEST03) {
+	std::string name = flnamed->name();
 	EXPECT_EQ("Test_Line", name);
 }
