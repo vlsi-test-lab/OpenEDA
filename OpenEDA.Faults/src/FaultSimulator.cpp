1248db39f05b0b85a0d9b9a6c0e610e1489e3348
Spencer Millican <skm0049@auburn.edu>
2019-07-17 17:55:01 +0000

    TDF Simulation fixed: X-states and multi-bit supported. Imported from Changeset 330

--- OpenEDA.Faults/src/FaultSimulator.cpp
+++ OpenEDA.Faults/src/FaultSimulator.cpp
@@ -21,6 +21,100 @@ FaultSimulator<_primitive>::~FaultSimulator() {
 	this->clearFaults();
 }
 
+/*
+ * Given a "faulty" value and the current line value, set what the faulty value
+ * will be when active (i.e., modifiy it) as well as a mask of what values may
+ * change in future fault simulation simulations (i.e., for TDFs).
+ *
+ * NOTE: inputs are modified through reference.
+ *
+ * @param _faultyValue The value to appear in the circuit during simulation.
+ * @param _lineValue The value of the line in the circuit (where the fault is).
+ * @param _mask The mask of values which may be affected for future iterations.
+ * @param _same When calculating the mask, do we want unknown circuit values to
+ *              be the same as or different than the fault? (FOR TDF: first vec
+ *              is true, second vec is false.
+ * @return Will the fault matter, i.e., will it have an effect on the circuit.
+ */
+template<class _primitive>
+bool maskAndModify(
+	Value<_primitive> & _faultyValue,
+	Value<_primitive> & _lineValue,
+	_primitive & _mask,
+	bool _same
+) {
+	//Any non-valid values on the line need to be made the opposite magnitude as the fault.
+	_primitive toForce = ~_lineValue.valid(); //The non-valid bits.
+	_primitive toFlip;
+	if (_same) {
+		toFlip = _lineValue.magnitude() ^ _faultyValue.magnitude();
+	} else {
+		toFlip = _lineValue.magnitude() ^ ~(_faultyValue.magnitude());
+	}
+	toFlip = toFlip & toForce;
+	_lineValue.magnitude(toFlip ^ _lineValue.magnitude());
+
+	if (_same) {
+		_mask = ~(_faultyValue.magnitude() ^ _lineValue.magnitude()) & _mask;
+	} else {
+		_mask = (_faultyValue.magnitude() ^ _lineValue.magnitude()) & _mask;
+	}
+
+	_primitive changedMagnitude = _mask; //It just so happened that the mask is the same as magnitudes the flip. Not necessarly the same as valids to flip.
+	_primitive changedValids = ~_lineValue.valid();
+	if (_same == false) {
+		_faultyValue.magnitude(_lineValue.magnitude() ^ changedMagnitude);
+		changedValids = (~changedValids ^ _faultyValue.valid()) & _mask;
+		_faultyValue.valid(_lineValue.valid() ^ changedValids);
+	} else {
+		changedValids = changedValids & _mask;
+		_faultyValue.valid(_faultyValue.valid() ^ changedValids);
+	}
+
+
+	return changedMagnitude | changedValids;
+}
+
+template<>
+bool maskAndModify(
+	Value<bool> & _faultyValue,
+	Value<bool> & _lineValue,
+	bool & _mask,
+	bool _same
+) {
+	//Any non-valid values on the line need to be made the opposite magnitude as the fault.
+	bool toForce = !_lineValue.valid(); //The non-valid bits.
+	bool toFlip;
+	if (_same) {
+		toFlip = _lineValue.magnitude() ^ _faultyValue.magnitude();
+	} else {
+		toFlip = _lineValue.magnitude() ^ !(_faultyValue.magnitude());
+	}
+	toFlip = toFlip & toForce;
+	_lineValue.magnitude(toFlip ^ _lineValue.magnitude());
+
+	if (_same) {
+		_mask = !(_faultyValue.magnitude() ^ _lineValue.magnitude()) & _mask;
+	} else {
+		_mask = (_faultyValue.magnitude() ^ _lineValue.magnitude()) & _mask;
+	}
+
+	bool changedMagnitude = _mask; //It just so happened that the mask is the same as magnitudes the flip. Not necessarly the same as valids to flip.
+	bool changedValids = !_lineValue.valid();
+	if (_same == false) {
+		_faultyValue.magnitude(_lineValue.magnitude() ^ changedMagnitude);
+		changedValids = (!changedValids ^ _faultyValue.valid()) & _mask;
+		_faultyValue.valid(_lineValue.valid() ^ changedValids);
+	} else {
+		changedValids = changedValids & _mask;
+		_faultyValue.valid(_faultyValue.valid() ^ changedValids);
+	}
+
+
+	return changedMagnitude | changedValids;
+}
+
+
 template<class _primitive>
 std::vector<Value<_primitive>> FaultSimulator<_primitive>::applyStimulus(
 	Circuit * _circuit, 
@@ -30,28 +124,12 @@ std::vector<Value<_primitive>> FaultSimulator<_primitive>::applyStimulus(
 	std::vector<SimulationNode<_primitive>*> _outputs,
 	std::vector<bool> _observe
 )
-{
-
-	std::unordered_set<Fault<_primitive>*> preProcessFaults;
-
-	//If TDF mode is enabled, do the preliminary fault elimination based on the
-	//current circuit state.
-	if (this->tdfEnable_ == true) {
-		for (Fault<_primitive>* fault : this->undetectedFaults_) {
-			if (this->notExcited(fault) == true) {
-				preProcessFaults.emplace(fault);
-			}
-		}
-	}
-	else {
-		preProcessFaults = this->undetectedFaults_;
-	}
-
+{	
+	//Populated (observable) output list (if not given)
 	if (_outputs.empty() == true) {
 		for (Levelized* output : _circuit->pos()) {
 			_outputs.push_back(dynamic_cast<SimulationNode<_primitive>*>(output));
 		}
-		//DELETE: doesn't work, won't compile. _outputs = std::vector<SimulationNode<T>*>(_circuit->pos().begin(), _circuit->pos().end());
 	}
 	if (_observe.empty() == true) {
 		_observe = std::vector<bool>(_outputs.size(), true);
@@ -59,21 +137,84 @@ std::vector<Value<_primitive>> FaultSimulator<_primitive>::applyStimulus(
 		throw "_outputs/_observe size mismach during fault simulation.";
 	}
 
+	//Chose (and conditionally modifiy) which faults to simulation.
+	//This structure contains the list of faults to simulate and the fault's original value:
+	// the fault's "value" may be changed during simulation if some bits of the faults will
+	// not have an impact during simulation. This is only needed because of TDF simulation.
+	//This structure also contains a "mask" of values to simulate.
+	std::vector<
+		std::pair<
+				Fault<_primitive>* , //The fault, .first
+				std::pair<
+					Value<_primitive>, //The fault's original value, .first.first
+					_primitive         //The mask of the fault (for s-a-f simulation, should be all 1s), .first.second
+				>
+		>
+	> faultsToSimulate;
+
+	for (Fault<_primitive>* fault : this->undetectedFaults_) {
+		Value<_primitive> faultValue = fault->value();
+		_primitive mask = (_primitive) 0xFFFFFFFFFFFFFFFF;
+		if (this->tdfEnable_ == true) {
+			Value<_primitive> newFaultValue = faultValue;
+			Value<_primitive> lineValue = fault->location()->value();
+			maskAndModify<_primitive>(newFaultValue, lineValue, mask, true);
+			fault->value(
+				std::vector <Value<_primitive>>(1, newFaultValue)
+			);
+		}
+
+		faultsToSimulate.push_back(
+			std::pair<Fault<_primitive>*, std::pair<Value<_primitive>, _primitive>>(
+				fault,
+				std::pair<Value<_primitive>, _primitive>(
+					faultValue, 
+					mask
+				)
+			)
+		);
+	}
+
+
+
 	//Obatin the "non-faulty" state.
 	std::vector<Value<_primitive>> goodOutputs  = this->Simulator<_primitive>::applyStimulus(_circuit, _stimulus, _simulationQueue, _inputs, _outputs, _observe);
 	//DELETE: not needed. this->Simulator<_primitive>::applyStimulus(_circuit,  _stimulus,  _simulationQueue, _inputs);
 	//DELETE: not needed. std::vector<Value<_primitive>> goodOutputs = this->outputs(_circuit);
 
 	//Obtain all faults which are "worth simulating".
-	std::unordered_set<Fault<_primitive>*> faults;
-	for (Fault<_primitive>* fault : preProcessFaults) {
-		if (this->hasImpact(fault) == true) {
-			faults.emplace(fault);
+	auto it = faultsToSimulate.begin();
+	while (it != faultsToSimulate.end()) {
+		std::pair<Fault<_primitive>*, std::pair<Value<_primitive>, _primitive > > entry = *it;
+		Fault<_primitive>* fault = entry.first;
+		Value<_primitive> faultValue = fault->value();
+		Value<_primitive> curentCircuitValue = fault->location()->value();
+		_primitive mask = entry.second.second;
+		if (
+			maskAndModify<_primitive>(faultValue, curentCircuitValue, mask, false) == true
+			) { //A chnage will be made by this fault.
+			fault->value(
+				std::vector<Value<_primitive>>(
+					1,
+					faultValue
+					)
+			);
+			++it;
+		
+		} else { //... else, remove it from the list (and restore the fault's effect).
+			Value<_primitive> originalFaultValue = entry.second.first;
+			fault->value(std::vector<Value<_primitive>>(
+				1,
+				originalFaultValue)
+			);
+			it = faultsToSimulate.erase(it);
 		}
 	}
 
+
 	//Simulate every fault.
-	for (Fault<_primitive>* fault : faults) {
+	for (auto entry : faultsToSimulate) {
+		Fault<_primitive>* fault = entry.first;
 		
 		_simulationQueue.add(fault->go()); //NOTE: "Go" will toggle the activation of the fault and return all new events from this (de)activation.
 		//DELETE do we need this? this->Simulator<_primitive>::applyStimulus(_circuit, _stimulus, _simulationQueue, _inputs);
@@ -84,6 +225,12 @@ std::vector<Value<_primitive>> FaultSimulator<_primitive>::applyStimulus(
 			this->detectedFaults_.emplace(fault);
 		}
 
+		Value<_primitive> originalFaultValue = entry.second.first;
+		fault->value(std::vector<Value<_primitive>>(
+			1,
+			originalFaultValue)
+			);
+		
 		_simulationQueue.add(fault->go()); //NOTE: "Go" will toggle the activation of the fault and return all new events from this (de)activation.
 		//DEBUG: I want to know ALL stimulus generated by a fault alone.
 		_simulationQueue.process();
